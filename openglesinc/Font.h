de9974d04ae11e1d14ff09c2f7e31e018aa6a428
Harold Serrano <harold.serrano@gmail.com>
2015-11-21 14:24:13 -0600

    update files

--- openglesinc/Font.h
+++ openglesinc/Font.h
@@ -3,7 +3,7 @@
 //  openglesinc
 //
 //  Created by Harold Serrano on 5/25/15.
-//  Copyright (c) 2015 www.roldie.com. All rights reserved.
+//  Copyright (c) 2015 www.haroldserrano.com. All rights reserved.
 //
 
 #ifndef __openglesinc__Font__
